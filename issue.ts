443f442b2753107a03a6047368809a2c731c1472
Ash Furrow <ash@ashfurrow.com>
2017-12-30 22:23:10 -0500

    Louder.

--- issue.ts
+++ issue.ts
@@ -1,5 +1,5 @@
 import { message, warning, danger } from "danger"
 
-message("Hello, from Peril!")
+warning("Hello, from Peril!")
 
 // TODO: Add a check to see if a changelog exists, then warn if it isn't in modified etc
