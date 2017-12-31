d4c84151da6887d9dc2d31a45b0b5b1bffe88317
Ash Furrow <ash@ashfurrow.com>
2017-12-30 23:34:07 -0500

    Removes testing code.

--- pr.ts
+++ pr.ts
@@ -1,7 +1,3 @@
 import { markdown, warn } from "danger"
 
-warn("Hello, from Peril!")
-markdown("Testing this too, hi!")
-fail("Sorry!")
-
 // TODO: Add a check to see if a changelog exists, then warn if it isn't in modified etc
